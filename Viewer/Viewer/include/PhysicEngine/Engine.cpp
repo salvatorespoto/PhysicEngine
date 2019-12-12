c6bc6f01fb6f1d048228c8b8d6105db7a399ddec
Salvatore Francesco Spoto <salvatore.spoto@gmail.com>
2019-12-12 23:31:30 +0100

    Refactor and document Engine class

--- Viewer/Viewer/include/PhysicEngine/Engine.cpp
+++ Viewer/Viewer/include/PhysicEngine/Engine.cpp
@@ -1,1 +1,85 @@
 #include "Engine.h"
+
+
+void RigidBodyEngine::Tick()
+{
+	DetectCollisions();
+	ProcessCollisions();
+	Update();
+}
+
+
+void RigidBodyEngine::SetStartingTime(float t)
+{
+	currentTime = t;
+}
+
+
+void RigidBodyEngine::SetTimeStep(float dt)
+{
+	timeStep = dt;
+}
+
+
+void RigidBodyEngine::AddRigidBody(RigidBody* rb)
+{
+	Objects.push_back(rb);
+}
+
+
+const std::vector<RigidBody*>& RigidBodyEngine::GetRigidBodies()
+{
+	return Objects;
+}
+
+
+const std::vector<Contact>& RigidBodyEngine::GetContacts()
+{
+	return Contacts;
+}
+
+
+void RigidBodyEngine::DetectCollisions()
+{
+	// Reset collisions
+	Contacts.clear();
+	for (RigidBody* rb : Objects)
+	{
+		rb->IsColliding = false;
+	}
+
+	// Detect collisions and compute contact sets
+	for (RigidBody* rbA : Objects)
+	{
+		for (RigidBody* rbB : Objects)
+		{
+			if (rbA == rbB) break;
+			std::vector<Contact> cs = TestRigidBodyIntersect(currentTime, timeStep, *rbA, *rbB);
+			if (!cs.empty())
+			{
+				rbA->IsColliding = true;
+				rbB->IsColliding = true;
+				for (Contact c : cs) Contacts.push_back(c);
+			}
+		}
+	}
+}
+
+
+void RigidBodyEngine::ProcessCollisions()
+{
+	if (Contacts.size() > 0)
+	{
+		DoCollisionResponse(currentTime, timeStep, Contacts, Objects);
+	}
+}
+
+
+void RigidBodyEngine::Update()
+{
+	for (RigidBody* b : Objects)
+	{
+		b->UpdateState(currentTime, timeStep);
+	}
+	currentTime += timeStep;
+}
--- Viewer/Viewer/include/PhysicEngine/Engine.h
+++ Viewer/Viewer/include/PhysicEngine/Engine.h
@@ -1,5 +1,4 @@
-#ifndef ENGINE_H
-#define ENGINE_H
+#pragma once
 
 #include <vector>
 
@@ -9,110 +8,108 @@
 #include "collision.h"
 
 
-/** The phisic engine */
+/**
+ * The physics engine namespace
+ *
+ * All the Physics engine classes are defined inside this namespace
+ */
 namespace PhysicEngine
 {
-	class Engine
+
+	/**
+	 * The physics engine main class
+	 *
+	 * This is the physics engine main class, it holds the list of all the objects, 
+	 * computes the effects of forces on objects, collisions detection,
+	 * collision responce and update the new object position accordingly.
+	 */
+	class RigidBodyEngine
 	{
-
-		private: 
-
-		/**
-		 * List of Rigid Body objects in the simulation
-		 */
-		std::vector<RigidBody*> Objects;
-
-		/**
-		 * Current contacts among objects
-		 */
-		std::vector<Contact> Contacts;
-
-		/**
-		 * Current simulation time
-		 */
-		float currentTime;
-
-		/**
-		 * Delta time between two engine updates 
-		 */
-		float timeStep;
-
 
 	public:
 
-		void Tick()
-		{
-			DetectCollisions();
-			ProcessCollsions();
-			Update();
-		}
+		/**
+		 * Execute the physics tick.
+		 *
+		 * This function execute the physics tick:
+		 * 1. apply extenal forces
+		 * 2. do collision detection
+		 * 3. compute collision response
+		 * 4. update the objects accordingly
+		 */
+		void Tick();
 
-		void SetStartingTime(float t) 
-		{
-			currentTime = t;
-		}
-		
-		void SetDeltaTime(float dt) 
-		{
-			timeStep = dt;
-		}
-
-
-		void AddRigidBody(RigidBody* c)
-		{
-			Objects.push_back(c);
-		}
-
-
-		void DetectCollisions()
-		{
-			Contacts.clear();
-			for (RigidBody* rb : Objects) rb->IsColliding = false;
-
-			for	(RigidBody* rbA : Objects)
-			{
-				for (RigidBody* rbB : Objects)
-				{
-					if (rbA == rbB) break;
-					std::vector<Contact> cs = TestRigidBodyIntersect(currentTime, timeStep, *rbA, *rbB);
-					if (!cs.empty())
-					{
-						rbA->IsColliding = true;
-						rbB->IsColliding = true;
-						for(Contact c : cs) Contacts.push_back(c);
-					}
-				}
-			}
-		}
-
-		void ProcessCollsions() 
-		{
-			if(Contacts.size() > 0) DoCollisionResponse(currentTime, timeStep,  Contacts, Objects);
-
-			/*for (Contact c : Contacts)
-
-			{
-				//ProcessCollidingContact(c.rb0, c.rb1, c.point, c.normal);
-			}*/
-		}
-
-		void Update() 
-		{
-			for (RigidBody* b : Objects)
-			{
-				b->UpdateState(currentTime, timeStep);
-			}
-			
-			currentTime += timeStep;
-		}
+		/** 
+		 * Set the simulation starting time
+		 *
+		 * @param t The simulation starting time
+		 */
+		void SetStartingTime(float t);
 
 		/**
-		 * Return all contacts among objects
+		 * Set the simulation step
+		 *
+		 * At each tick the simulation is updated of a time step dt
+		 *
+		 * @param dt The time step
 		 */
-		const std::vector<Contact>& GetContacts()
-		{
-			return Contacts;
-		}
+		void SetTimeStep(float dt);
+
+		/**
+		 * Add a rigid body to the scene
+		 *
+		 * @param rb A pointer to a RigidBody object
+		 */
+		void AddRigidBody(RigidBody* rb);
+
+		/**
+		 * Return a vector of RigidBody objects in the scene
+		 *
+		 * @return A vector of RigidBody pointers to the objects in the scene
+		 */
+		const std::vector<RigidBody*>& GetRigidBodies();
+
+		/**
+		 * Return a vector of the Contacts among objects in the scene
+		 *
+		 * @return A vector of Contacts, these are the contact among all objects in the scene
+		 */
+		const std::vector<Contact>& GetContacts();
+
+
+	private: 
+
+		float currentTime;					/**< Current simulation time */
+		float timeStep;						/**< The simulation time step */
+
+		std::vector<RigidBody*> Objects; 	/**< List of Rigid Body objects in the scene */
+		std::vector<Contact> Contacts;		/**< Current detected contacts among objects */
+		 
+		/**
+		 * Detect collsion among objects in the scene
+		 *
+		 * This function test all object for collisions and compute the contact sets of 
+		 * colliding object, filling the Contacts vector property.
+		 * The contact list is resetted at each tick.
+		 */
+		void DetectCollisions();
+
+		/**
+		 * Compute all collisions responses among objects 
+		 *
+		 * This function compute all resulting forces from objects interactions, 
+		 * both colliding contacts and resting contacts.
+		 */
+		void ProcessCollisions();
+
+		/** 
+		 * Update
+		 *
+		 * This function compute the updated state after a time "timeStep" from the previous
+		 * tick. 
+		 * The updated state is the result of the collision processed in ProcessCollision and
+		 * the extenaral enviroment forces (e.g. gravity). 
+		 */
+		void Update();
 	};
 }
-#endif
